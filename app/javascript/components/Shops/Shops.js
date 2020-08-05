1779c0571c2ab4a414b1c3cbbf53cd830100d177
Shaneil Clarke <shaneil.clarke@cutover.com>
2020-08-05 14:55:34 +0100

    updated styling on components

--- app/javascript/components/Shops/Shops.js
+++ app/javascript/components/Shops/Shops.js
@@ -1,81 +1,81 @@
 import React, { useState, useEffect} from 'react';
 import axios from 'axios';
 import Shop from './Shop';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import ShopForm from '../Shops/ShopsForm';
 
-const Home = styled.div`
-  text-align: center;
-  width: 100%;
-  margin: 0;
-  color: #4C5656;
-  overflow-x: hidden;
-`;
+// const Home = styled.div`
+//   text-align: center;
+//   width: 100%;
+//   margin: 0;
+//   color: #4C5656;
+//   overflow-x: hidden;
+// `;
 
-const Header = styled.div`
-  position: fixed;
-  top: 0;
-  width: 100%;
-  color: #fff;
-  background-color: #19CACA;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  h1 {
-    font-size: 3em;
-    text-align: left;
-    padding: 20px;
-    margin: 0;
+// const Header = styled.div`
+//   position: fixed;
+//   top: 0;
+//   width: 100%;
+//   color: #fff;
+//   background-color: #19CACA;
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   h1 {
+//     font-size: 3em;
+//     text-align: left;
+//     padding: 20px;
+//     margin: 0;
 
-  }
-  ul {
-    list-style-type: none;
-    display: flex;
-  }
-  li {
-    flex-direction: row;
-    justify-content: space-between;
-  }
-`;
+//   }
+//   ul {
+//     list-style-type: none;
+//     display: flex;
+//   }
+//   li {
+//     flex-direction: row;
+//     justify-content: space-between;
+//   }
+// `;
 
-const Subheader = styled.p`
-  font-weight:300;
-  font-size: 1em;
-  padding: 20px;
-`;
+// const Subheader = styled.p`
+//   font-weight:300;
+//   font-size: 1em;
+//   padding: 20px;
+// `;
 
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(1, 1fr);
-  grid-gap: 20px;
-  width: 100%;
-  > div {
-    background-color: #fff;
-    border-radius: 5px;
-    padding: 20px;
-  }
-`;
+// const Grid = styled.div`
+//   display: grid;
+//   grid-template-columns: repeat(1, 1fr);
+//   grid-gap: 20px;
+//   width: 100%;
+//   > div {
+//     background-color: #fff;
+//     border-radius: 5px;
+//     padding: 20px;
+//   }
+// `;
 
-const Wrapper = styled.div`
-  margin-left: 20px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-gap: 20px;
-  width: 100%;
-`;
+// const Wrapper = styled.div`
+//   margin-left: 20px;
+//   display: grid;
+//   grid-template-columns: repeat(2, 1fr);
+//   grid-gap: 20px;
+//   width: 100%;
+// `;
 
-const Column = styled.div`
-  background-color: #fff;
-  height: 100vh;
-  overflow: scroll;
-  &:last-child {
-    background: #f7f7f7;
-  }
-`;
+// const Column = styled.div`
+//   background-color: #fff;
+//   height: 100vh;
+//   overflow: scroll;
+//   &:last-child {
+//     background: #f7f7f7;
+//   }
+// `;
 
-const Main = styled.div`
-  left-padding: 50px;
-`;
+// const Main = styled.div`
+//   left-padding: 50px;
+// `;
 
 const Shops = () => {
   const [shops, setShops] = useState([]);
@@ -109,34 +109,93 @@ const Shops = () => {
     )
   });
   return (
-    <Home >
-      <Header>
-        <h1>Eco Shopping</h1>
-        <ul>
-          <li>Home</li>
-          <li>Shops</li>
-          <li>Learn More</li>
-        </ul>
-      </Header>
-      <Subheader>Eco friendly shopping. Find shops that use little or no plastic packaging</Subheader>
-      <Wrapper>
-        <Column>
-          <Grid>
+    <div>
+      <header>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light" >
+          <a className="navbar-brand" href="#"><h1>Eco Shopping</h1></a>
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item"></li>
+            <li className="nav-item"></li>
+            <li className="nav-item"></li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="text" placeholder="Email/Username" aria-label="email" />
+            <input className="form-control mr-sm-2" type="password" placeholder="Password" aria-label="password" />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Log In</button>
+          </form>
+        </nav>
+
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Shops <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Learning Center</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+
+      <section>
+        <div className="jumbotron" style={{ backgroundColor: "rgb(25, 202, 202)"}}>
+          <h2 className="text-center text-white">Eco friendly shopping. Find shops that use little or no plastic packaging</h2>
+        </div>
+      </section>
+
+      <section>
+        <div className="row">
+          <div className="col">
             {grid}
-          </Grid>
-        </Column>
-        <Column>
-          <ShopForm
-            handleChange={handleChange}
-            handleSubmit={handleSubmit}
-            attributes={shops.attributes}
-          />
-        </Column>
-      </Wrapper>
-    </Home>
-
-
+          </div>
+          <div className="col">
+              <ShopForm
+              handleChange={handleChange}
+              handleSubmit={handleSubmit}
+              attributes={shops.attributes}
+              />
+          </div>
+        </div>
+      </section>
+    </div>
   )
+  {/* // return (
+  //   <Home >
+  //     <Header>
+  //       <h1>Eco Shopping</h1>
+  //       <ul>
+  //         <li>Home</li>
+  //         <li>Shops</li>
+  //         <li>Learn More</li>
+  //       </ul>
+  //     </Header>
+  //     <Subheader>Eco friendly shopping. Find shops that use little or no plastic packaging</Subheader>
+  //     <Wrapper>
+  //       <Column>
+  //         <Grid>
+  //           {grid}
+  //         </Grid>
+  //       </Column>
+  //       <Column>
+  //         <ShopForm
+  //           handleChange={handleChange}
+  //           handleSubmit={handleSubmit}
+  //           attributes={shops.attributes}
+  //         />
+  //       </Column>
+  //     </Wrapper>
+  //   </Home>
+
+
+  // ) */}
 }
 
 export default Shops
--- app/javascript/components/Shops/ShopsForm.js
+++ app/javascript/components/Shops/ShopsForm.js
@@ -1,119 +1,155 @@
 import React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-const Wrapper = styled.div`
-  margin-left: auto;
-  margin-right: auto;
-  display: grid;
-  grid-template-columns: repeat(1, 1fr);
-`;
+// const Wrapper = styled.div`
+//   margin-left: auto;
+//   margin-right: auto;
+//   display: grid;
+//   grid-template-columns: repeat(1, 1fr);
+// `;
 
-const Column = styled.div`
-  background-color: #fff;
-  height: 100vh;
-  overflow: scroll;
+// const Column = styled.div`
+//   background-color: #fff;
+//   height: 100vh;
+//   overflow: scroll;
 
-  &:last-child {
-    background: #000;
-  }
-`;
+//   &:last-child {
+//     background: #000;
+//   }
+// `;
 
-const ButtonWrapper = styled.div`
-  margin: 30px 0 20px 0;
-  height:50px;
-  button {
-    font-size: 0.8em;
-    color: #fff;
-    background-color: #19CACA;
-    border: 1px solid #19CACA;
-    padding: 10px 50px;
-    cursor: pointer;
-    border-radius: 50px;
-    text-align: center;
-    line-height: 20px;
-    min-height: 40px;
-    margin: 7px;
-    font-weight: 600;
-    text-decoration: none;
-    width: auto;
-    transition: ease-in-out 0.1s;
-    &:hover{
-      border-color: rgb(25, 153, 166);
-      background: rgb(25, 153, 166);
-    }
-  }
-`;
+// const ButtonWrapper = styled.div`
+//   margin: 30px 0 20px 0;
+//   height:50px;
+//   button {
+//     font-size: 0.8em;
+//     color: #fff;
+//     background-color: #19CACA;
+//     border: 1px solid #19CACA;
+//     padding: 10px 50px;
+//     cursor: pointer;
+//     border-radius: 50px;
+//     text-align: center;
+//     line-height: 20px;
+//     min-height: 40px;
+//     margin: 7px;
+//     font-weight: 600;
+//     text-decoration: none;
+//     width: auto;
+//     transition: ease-in-out 0.1s;
+//     &:hover{
+//       border-color: rgb(25, 153, 166);
+//       background: rgb(25, 153, 166);
+//     }
+//   }
+// `;
 
-const UserInput = styled.input`
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 10px 0;
-  display: inline-block;
-  border: none;
-  background-color: #f1f1f1;
-  text-align: center;
-  font-size: 0.7em;
-`;
+// const UserInput = styled.input`
+//   width: 100%;
+//   padding: 15px;
+//   margin: 5px 0 10px 0;
+//   display: inline-block;
+//   border: none;
+//   background-color: #f1f1f1;
+//   text-align: center;
+//   font-size: 0.7em;
+// `;
 
-const Field = styled.div`
-  margin-left: 50px;
-  margin-right: 80px;
-  &:first-child{
-    margin-top: 30px;
-  }
-`;
+// const Field = styled.div`
+//   margin-left: 50px;
+//   margin-right: 80px;
+//   &:first-child{
+//     margin-top: 30px;
+//   }
+// `;
 
-const Note = styled.p`
-  font-size: 0.7em;
-  font-weight: italic;
-  margin-bottom: 10px;
-`;
+// const Note = styled.p`
+//   font-size: 0.7em;
+//   font-weight: italic;
+//   margin-bottom: 10px;
+// `;
 
-const Label = styled.label`
-  font-weight: 600;
-  margin-bottom: 20px;
-`;
+// const Label = styled.label`
+//   font-weight: 600;
+//   margin-bottom: 20px;
+// `;
+
 const ShopsForm = (props) => {
   return (
-    <Wrapper>
-      <form onSubmit={props.handleSubmit}>
-        <h3>Fill in details to add a shop</h3>
-        <Note>* mandatory fields</Note>
-        <Field>
-          <Label htmlFor="shop_name">Business Name*: </Label>
-          <UserInput onChange={props.handleChange} value={props.shops} type="text" name="shop_name" id="shop_name" placeholder="ABC Market"></UserInput>
-        </Field>
-        <Field>
-          <Label htmlFor="address_1">Street Address*: </Label>
-          <UserInput onChange={props.handleChange} value={props.shops} type="text" name="address_1" id="address_1" placeholder="123 Greenwich Road"></UserInput>
-        </Field>
-        <Field>
-          <Label htmlFor="address_2">Unit Address: </Label>
-          <UserInput onChange={props.handleChange} value={props.shops} type="text" name="address_2" id="address_2" placeholder="Unit 2"></UserInput>
-        </Field>
-        <Field>
-          <Label htmlFor="town">Town: </Label>
-          <UserInput onChange={props.handleChange} value={props.shops} type="text" name="town" id="town" placeholder="Greenwich"></UserInput>
-        </Field>
-        <Field>
-          <Label htmlFor="city">City: </Label>
-          <UserInput onChange={props.handleChange} value={props.shops} type="text" name="city" id="city" placeholder="London"></UserInput>
-        </Field>
-        <Field>
-          <Label htmlFor="post_code">Post code*: </Label>
-          <UserInput onChange={props.handleChange} value={props.shops} type="text" name="post_code" id="post_code" placeholder="SE3 6PQ"></UserInput>
-        </Field>
-        <Field>
-          <Label htmlFor="website">Website: </Label>
-          <UserInput onChange={props.handleChange} value={props.shops} type="text" name="website" id="website" placeholder="https://www.google.com"></UserInput>
-        </Field>
-        <p>By adding a shop you agree to our <a href="#">terms and privacy</a> </p>
-        <ButtonWrapper>
-          <button type="submit">Add Shop</button>
-        </ButtonWrapper>
-      </form>
+    <form onSubmit={props.handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="name">Business name*</label>
+        <input type="text" className="form-control" id="name" name="shop_name" placeholder="ABC Market" onChange={props.handleChange} value={props.shops}/>
+      </div>
+      <div className="form-group">
+        <label htmlFor="address_1">Business address*</label>
+        <input type="text" className="form-control" id="address_1" name="address_1" placeholder="123 Random Road" onChange={props.handleChange} value={props.shops}/>
+      </div>
+      <div className="form-group">
+        <label htmlFor="address_2">Business unit/no.</label>
+        <input type="text" className="form-control" id="address_2" name="address_2" placeholder="Unit 23" onChange={props.handleChange} value={props.shops}/>
+      </div>
+      <div className="form-group">
+        <label htmlFor="town">Town</label>
+        <input type="text" className="form-control" id="town" name="town" placeholder="Hackney" onChange={props.handleChange} value={props.shops}/>
+      </div>
+      <div className="form-group">
+        <label htmlFor="city">City</label>
+        <input type="text" className="form-control" id="city" name="city" placeholder="London" onChange={props.handleChange} value={props.shops}/>
+      </div>
+      <div className="form-group">
+        <label htmlFor="post_code">Postcode</label>
+        <input type="text" className="form-control" id="post_code" name="post_code" placeholder="E23 6PQ" onChange={props.handleChange} value={props.shops}/>
+      </div>
+      <div className="form-group">
+        <label htmlFor="website">Website</label>
+        <input type="text" className="form-control" id="website" name="website" placeholder="https://www.google.com" onChange={props.handleChange} value={props.shops}/>
+      </div>
+      <div className="form-group form-check">
+        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+        <label className="form-check-label" htmlFor="exampleCheck1">By ticking, you agree to terms and conditions</label>
+      </div>
+      <button type="submit" className="btn btn-primary">Submit Shop</button>
+    </form>
+    // <Wrapper>
+    //   <form onSubmit={props.handleSubmit}>
+    //     <h3>Fill in details to add a shop</h3>
+    //     <Note>* mandatory fields</Note>
+    //     <Field>
+    //       <Label htmlFor="shop_name">Business Name*: </Label>
+    //       <UserInput onChange={props.handleChange} value={props.shops} type="text" name="shop_name" id="shop_name" placeholder="ABC Market"></UserInput>
+    //     </Field>
+    //     <Field>
+    //       <Label htmlFor="address_1">Street Address*: </Label>
+    //       <UserInput onChange={props.handleChange} value={props.shops} type="text" name="address_1" id="address_1" placeholder="123 Greenwich Road"></UserInput>
+    //     </Field>
+    //     <Field>
+    //       <Label htmlFor="address_2">Unit Address: </Label>
+    //       <UserInput onChange={props.handleChange} value={props.shops} type="text" name="address_2" id="address_2" placeholder="Unit 2"></UserInput>
+    //     </Field>
+    //     <Field>
+    //       <Label htmlFor="town">Town: </Label>
+    //       <UserInput onChange={props.handleChange} value={props.shops} type="text" name="town" id="town" placeholder="Greenwich"></UserInput>
+    //     </Field>
+    //     <Field>
+    //       <Label htmlFor="city">City: </Label>
+    //       <UserInput onChange={props.handleChange} value={props.shops} type="text" name="city" id="city" placeholder="London"></UserInput>
+    //     </Field>
+    //     <Field>
+    //       <Label htmlFor="post_code">Post code*: </Label>
+    //       <UserInput onChange={props.handleChange} value={props.shops} type="text" name="post_code" id="post_code" placeholder="SE3 6PQ"></UserInput>
+    //     </Field>
+    //     <Field>
+    //       <Label htmlFor="website">Website: </Label>
+    //       <UserInput onChange={props.handleChange} value={props.shops} type="text" name="website" id="website" placeholder="https://www.google.com"></UserInput>
+    //     </Field>
+    //     <p>By adding a shop you agree to our <a href="#">terms and privacy</a> </p>
+    //     <ButtonWrapper>
+    //       <button type="submit">Add Shop</button>
+    //     </ButtonWrapper>
+    //   </form>
 
-    </Wrapper>
+    // </Wrapper>
   )
 }
 
